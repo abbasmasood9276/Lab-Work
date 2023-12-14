6be470b672fe7c7429f6ae384c0e2e46bf8fb7e5
abbasmasood9276 <145642179+abbasmasood9276@users.noreply.github.com>
2023-12-14 09:25:49 +0500

    upload

---
+++ Interpreter/SonExpression.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interpreter;
+
+/**
+ *
+ * @author fa20-bse-001
+ */
+public class SonExpression implements Expression {
+    private Expression parent;
+    private Expression child;
+
+    public SonExpression(Expression parent, Expression child) {
+        this.parent = parent;
+        this.child = child;
+    }
+
+    @Override
+    public boolean interpret(String context) {
+      
+        return parent.interpret(context) && child.interpret(context);
+    }
+}
+
